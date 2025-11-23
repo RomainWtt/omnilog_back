d4ccd1fd630a57259ba719776d7a1f1d5da60105
Romain Wautot <romain.wautot@gmail.com>
2025-11-23 16:56:44 +0100

    fix/text pipeline

--- app/api/router.py
+++ app/api/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.endpoints import auth, users, media, library, review, oauth, email_verification
 from app.api.endpoints import friendship
+from app.api.endpoints import report
 
 api_router = APIRouter()
 
@@ -10,7 +11,7 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(media.router, prefix="/media", tags=["media"])
 api_router.include_router(library.router, prefix="/library", tags=["library"])
 api_router.include_router(review.router, prefix="/review", tags=["review"])
-api_router.include_router(review.router, prefix="/review_reports", tags=["report"])
+api_router.include_router(report.router, prefix="/review_reports", tags=["report"])
 api_router.include_router(oauth.router, prefix="/oauth")
 api_router.include_router(email_verification.router, prefix="/email", tags=["Email Verification"])
 api_router.include_router(friendship.router, prefix="/friendships", tags=["friendship"])
